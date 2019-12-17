816bb522506b22a9bbbe2624e71dfb32b5799fce
Mozafari1 <m_rahmat@hotmail.no>
2019-12-17 18:31:18 +0100

    testing

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,12 +1,15 @@
+// Import the React and ReactDOM libaries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a react component " A React Component is a function or a class that generates HTML to show the user and handles feedback from the userr"
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const App = () => { // this is 20215 arrow function
+    return <div > Test < /div>;
+};
+
+
+// Take the react component and show it on the screen
+
+ReactDOM.render( < App / > , document.querySelector('#root'));
