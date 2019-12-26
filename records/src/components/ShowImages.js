5e6e50449ecb369c83cca496cb60edfe84f07567
Mozafari1 <m_rahmat@hotmail.no>
2019-12-26 20:51:12 +0100

    App v2

--- records/src/components/ShowImages.js
+++ records/src/components/ShowImages.js
@@ -1,9 +1,10 @@
+import './ShowImages.css';
 import React from 'react';
-
+import ImageCard from './ImageCard';
 const ShowImages = (props) => {
-  const images = props.images.map(({ description, id, urls }) => {
-    return <img key={id} src={urls.regular} alt={description} />;
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
   });
-  return <div>{images}</div>;
+  return <div className='show-images'>{images}</div>;
 };
 export default ShowImages;
