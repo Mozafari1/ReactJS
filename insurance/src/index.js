43ddfad265c33b0d6a53a15501224677146cc17c
Mozafari1 <m_rahmat@hotmail.no>
2019-12-27 20:46:20 +0100

    insurance

--- insurance/src/index.js
+++ insurance/src/index.js
@@ -10,4 +10,48 @@ const App = (name, amount) => {
         }
     };
 };
+const deletePolicy = (name) => {
+    return {
+        type: 'DELETE_POLICY',
+        payload: {
+            name
+        }
+    };
+};
+const createClaim = (name, amountOfMoney) => {
+    return {
+        type: 'CREATE_CLAIM',
+        paylaod: {
+            name,
+            amountOfMoney
+        }
+    };
+};
+
+const claimHistory = (oldHistory = [], action) => {
+    if (action.type === 'CREATE_CLAIM') {
+        return [...oldHistory, action.paylaod];
+    }
+    return oldHistory;
+};
+
+const accounting = (Money = 10000, action) => {
+    if (action.type === 'CREATE_CLAIM') {
+        return Money - action.paylaod.amountOfMoney;
+    }
+    els;
+    if (action.type == 'CREATE_CLAIM') {
+        return Money + action.paylaod.amount;
+    }
+    return Money;
+};
+
+const policies = (listOfPolices = [], action) => {
+    if (action.type === 'CREATE_CLAIM') {
+        return [...listOfPolices, action.paylaod.name];
+    } else if (action.type === 'DELETE_POLICY') {
+        return listOfPolices.filter((name) => name !== action.paylaod.name);
+    }
+    return listOfPolices;
+};
 ReactDOM.render( < App / > , document.querySelector('#root'));
