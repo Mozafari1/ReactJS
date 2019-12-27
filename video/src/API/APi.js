a5b007f69ae76b5536b09a5a0201a9a8e8ffbe27
Mozafari1 <m_rahmat@hotmail.no>
2019-12-27 20:31:41 +0100

    utube api

--- video/src/API/APi.js
+++ video/src/API/APi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyC9FG9ZudX732azBhNmfPpZSbAP3U7LBTs';
+const KEY = 'YOUR KEY';
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
